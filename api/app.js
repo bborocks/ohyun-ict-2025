a8202993913b360d4292fcaf6c69acd26514c9b6
bborocks <bborock62@gmail.com>
2025-06-12 20:45:44 +0900

    sns

--- api/app.js
+++ api/app.js
@@ -8,14 +8,37 @@
 
 // async function init(){}
 const post_url =
-  "https://crudcrud.com/api/4cfcb8e6a4934d4eb3f28e4b84657416/post";
+  "https://crudcrud.com/api/05806477258e4d7ab576f3c40db6f10f/post";
 const init = async () => {
   const res = await fetch(
-    "https://crudcrud.com/api/4cfcb8e6a4934d4eb3f28e4b84657416/post"
+    "https://crudcrud.com/api/05806477258e4d7ab576f3c40db6f10f/post"
   );
   const json = await res.json();
   console.log(json);
+  json.forEach(post =>{
+    const postLi = document.createElement("li")
+    postLi.innerText = post.title
+    document.querySelector("#board").appendChild(postLi)
+
+    const contentDiv = document.createElement("div")
+    contentDiv.innerText = post.content;
+    postLi.appendChild(contentDiv)
+
+    contentDiv.style.display = 'none'
+
+    postLi.addEventListener('click',()=>{
+      if (contentDiv.style.display == 'block'){
+        contentDiv.style.display = 'none'
+      }
+      else{
+        contentDiv.style.display = "block"
+      }
+    })
+
+  })
 };
+
+
 
 const write = async (data) => {
   const res = await fetch(post_url, {
@@ -42,5 +65,13 @@ document.querySelector("#post-form").addEventListener("submit", (e) => {
   e.preventDefault();
   submit();
 });
+document.querySelector("#toggle-write").addEventListener("click", () =>{
+  if(document.querySelector("#post-form").style.display == 'block'){
+    document.querySelector("#post-form").style.display = 'none'
+  }
+  else{
+    document.querySelector("#post-form").style.display = 'block'
+  }
+})
 
 init();
